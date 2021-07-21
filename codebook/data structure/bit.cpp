300a0e15ef8a7384d3f3ec9fbffa27993ebfec0e
iceysheep157 <oosheepyerd79135@gmail.com>
2021-07-21 15:15:49 +0800

    add DS

---
+++ codebook/data structure/bit.cpp
@@ -0,0 +1,14 @@
+struct BIT {
+	static const int mxN = 2e5 + 50;
+	#define low(x) (x & -x)
+	int bit[mxN], n;
+	inline init() { memset(bit, 0, sizeof(bit)); }
+	inline int qry(int x) {
+		int ret = 0;
+		for(; x > 0; x -= low(x)) ret += bit[x];
+		return ret;
+	}
+	inline void modi(int x, int v) {
+		for(; x <= n; x += low(x)) bit[x] += v;
+	}
+};
