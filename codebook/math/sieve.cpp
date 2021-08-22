d6dd26030d3b3823b5d7389dd118d45571fce638
iceysheep157 <oosheepyerd79135@gmail.com>
2021-08-22 15:33:22 +0800

    add sieve

---
+++ codebook/math/sieve.cpp
@@ -0,0 +1,21 @@
+vector<int> prime;
+int mu[maxn], fac[maxn];
+void sieve(int mxN) {
+	// This is a O(N) sieve
+	for(int i = 2; i <= mxN; i ++) {
+		if(!fac[i]) {
+			fac[i] = i;
+			mu[i] = -1;
+			prime.push_back(i);
+		}
+		for(int j : prime) {
+			if(i * j > mxN) break;
+			fac[i * j] = j;
+			mu[i * j] = -mu[i];
+			if(i % j == 0) {
+				mu[i * j] = 0;
+				break;
+			}
+		}
+	}
+}
