46a6fac59c8d095ce58627a1d221ae278fbad61d
iceysheep157 <oosheepyerd79135@gmail.com>
2021-09-19 11:52:58 +0800

    fxxk 0-base

--- codebook/math/Matmul.cpp
+++ codebook/math/Matmul.cpp
@@ -1,9 +1,9 @@
 struct Mat {
-	static const int N = 50;
+	static const int N = 55;
 	int r, c, m[N][N];
 	Mat(){}
 	Mat(int _r, int _c) { 
-		r = _r; c = _c;
+		r = _r + 1; c = _c + 1;
 		memset(m, 0, sizeof(m));
 	}
 	int* operator[](int x) {
@@ -11,9 +11,9 @@ struct Mat {
     }
 	Mat operator+(Mat a) {
 		Mat ret(r, c);
-		for(int i = 0; i < r; i ++) {
-			for(int j = 0; j < c; j ++) {
-				ret.m[i][j] = m[i][j] + a.m[i][j];
+		for(int i = 1; i <= r; i ++) {
+			for(int j = 1; j <= c; j ++) {
+				ret[i][j] = m[i][j] + a[i][j];
 			}
 		}
 		return ret;
@@ -21,9 +21,9 @@ struct Mat {
 	Mat operator*(Mat a) {
 		assert(c == a.r);
 		Mat ret(r, a.c);
-		for(int k = 0; k < c; k ++) {
-			for(int i = 0; i < r; i ++) {
-				for(int j = 0; j < a.c; j ++) {
+		for(int k = 1; k <= c; k ++) {
+			for(int i = 1; i <= r; i ++) {
+				for(int j = 1; j <= a.c; j ++) {
 					ret.m[i][j] += m[i][k] * a.m[k][j];
 				}
 			}
@@ -31,8 +31,8 @@ struct Mat {
 		return ret;
 	}
 	void print() {
-		for(int i = 0; i < r; i ++) {
-			for(int j = 0; j < c; j ++) {
+		for(int i = 1; i <= r; i ++) {
+			for(int j = 1; j <= c; j ++) {
 				cout << m[i][j] << ' ';
 			}
 			cout << '\n';
