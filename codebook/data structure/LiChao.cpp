f0f33b56352e0c8427e306e105730cd0b791237b
iceysheep157 <oosheepyerd79135@gmail.com>
2022-01-27 17:46:28 +0800

    Li Chao tree add_segment

--- codebook/data structure/LiChao.cpp
+++ codebook/data structure/LiChao.cpp
@@ -8,7 +8,8 @@ struct segment {
 struct LiChao {
     #define ls (idx << 1)
     #define rs (idx << 1 | 1)
-    void add(segment seg, int l = 1, int r = 5e4, int idx = 1) {
+    const static int N = 5e4;
+    void add_line(segment seg, int l = 1, int r = N, int idx = 1) {
         if(l == r) {
             if(seg.val(l) > tr[idx].val(l)) tr[idx] = seg;
             return;
@@ -17,11 +18,20 @@ struct LiChao {
         if(seg.m < tr[idx].m) swap(seg, tr[idx]);
         if(seg.val(md) > tr[idx].val(md)) {
             swap(seg, tr[idx]);
-            add(seg, l, md, ls);
+            add_line(seg, l, md, ls);
         }
-        else add(seg, md + 1, r, rs);
+        else add_line(seg, md + 1, r, rs);
     }
-    double qry(int v, int l = 1, int r = 5e4, int idx = 1) {
+    void add_segment(segment seg, int a, int b, int l = 1, int r = N, int idx = 1) {
+        if(a <= l and b >= r) {
+            add_line(seg, l, r, idx);
+            return;
+        }
+        int md = (l + r) >> 1;
+        if(a <= md) add_segment(seg, a, b, l, md, ls);
+        if(b > md) add_segment(seg, a, b, md + 1, r, rs);
+    }
+    double qry(int v, int l = 1, int r = N, int idx = 1) {
         if(l == r) return tr[idx].val(v);
         int md = (l + r) >> 1;
         double ret = tr[idx].val(v);
