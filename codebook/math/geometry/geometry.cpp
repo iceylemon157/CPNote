dc1d3099b72eee5b337f489dccd55fea84d44e81
iceysheep157 <oosheepyerd79135@gmail.com>
2022-01-27 17:46:01 +0800

    update geometry / merge files

--- codebook/math/geometry/geometry.cpp
+++ codebook/math/geometry/geometry.cpp
@@ -1,26 +1,48 @@
-typedef complex<double> point;
-typedef complex<double> Vector;
+typedef complex<double> pdd;
 #define x real()
 #define y imag()
+const double eps = 1e-10;
 
-inline double dot(Vector a, Vector b) {	return (conj(a) * b).x; }
-inline double cross(Vector a, Vector b) { return (conj(a) * b).y; }
-inline double cross(point a, point o, point b) { return cross(a - o, b - o); }
-inline double dist(point a, point b) { return abs(a - b); }
-inline double dist(point a, point b, point c, bool issegment = false) {
-	double d = cross(b - a, c - a) / dist(a, b);
-	if(issegment) {
-		double dt = dot(b - a, c - b);
-		if(dt > 0) return dist(b, c);
-		dt = dot(a - b, c - a);
-		if(dt > 0) return dist(a, c);
-	}
-	return abs(d);
+inline double dot(pdd a, pdd b) { return (conj(a) * b).x; }
+inline double cross(pdd a, pdd b) { return (conj(a) * b).y; }
+inline double cross(pdd a, pdd o, pdd b) { return cross(a - o, b - o); }
+inline double dist(pdd a, pdd b) { return abs(a - b); }
+inline double dist(pdd a, pdd b, pdd c, bool issegment = false) {
+    double d = cross(b - a, c - a) / dist(a, b);
+    if(issegment) {
+        double dt = dot(b - a, c - b);
+        if(dt > 0) return dist(b, c);
+        dt = dot(a - b, c - a);
+        if(dt > 0) return dist(a, c);
+    }
+    return abs(d);
+}
+int sign(double a) {
+    return (fabs(a) < eps? 0 : (a > 0? 1 : -1));
+}
+int ori(pdd a, pdd b, pdd c) {
+    // vector ab to vector ac
+    return sign(cross(b - a, c - a));
 }
 template<class T>
 istream& operator>> (istream& is, complex<T>& p) {
-	T a, b;
-	is >> a >> b;
-	p = {a, b};
-	return is;
+    T a, b;
+    is >> a >> b;
+    p = {a, b};
+    return is;
 } 
+pdd p[maxn];
+
+bool segment_intersection(pdd a, pdd b, pdd c, pdd d) {
+    for(int i = 1; i <= 2; i ++) {
+        double da = cross(b - a, c - a);
+        double db = cross(b - a, d - a);
+        if((da > 0.0 and db > 0.0) or (da < 0.0 and db < 0.0)) return false;
+        if(da == 0.0 and db == 0.0) {
+            if(max(a.x, b.x) < min(c.x, d.x) or max(a.y, b.y) < min(c.y, d.y)) return false;
+        }
+        swap(a, c);
+        swap(b, d);
+    }
+    return true;
+}
