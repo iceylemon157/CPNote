9eddb181a41820942607ef03cb90bd3a20e858ba
iceylemon157 <oosheepyerd79135@gmail.com>
2024-05-04 03:40:41 +0800

    fix: update coding style

--- codebook/string/z value.cpp
+++ codebook/string/z value.cpp
@@ -5,9 +5,9 @@ inline void buildz(string s) {
     z.resize(n);
     z[0] = n;
     int l = 0, r = n - 1;
-    for(int i = 1; i < (int)s.size(); i ++) {
+    for (int i = 1; i < (int)s.size(); i ++) {
         z[i] = max(min(z[i - l], r - i), 0ll);
-        while(i + z[i] < n and s[z[i]] == s[i + z[i]]) {
+        while (i + z[i] < n and s[z[i]] == s[i + z[i]]) {
             l = i, r = i + z[i], z[i] ++;
         }
     }
@@ -17,8 +17,8 @@ inline int count(string a, string b) {
     string s = b + "$" + a;
     int cnt = 0;
     buildz(s);
-    for(int i = 0; i < (int)s.size(); i ++) {
-        if(z[i] == (int)b.size()) cnt ++;
+    for (int i = 0; i < (int)s.size(); i ++) {
+        if (z[i] == (int)b.size()) cnt ++;
     }
     return cnt;
 }
